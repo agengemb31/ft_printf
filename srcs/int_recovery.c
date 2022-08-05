2993623481531160eca4e5c427b27663807f6e8f
UhtredTheDane <32024540+UhtredTheDane@users.noreply.github.com>
2022-08-05 23:43:37 +0200

    Update int_recovery.c

--- srcs/int_recovery.c
+++ srcs/int_recovery.c
@@ -12,17 +12,17 @@
 
 #include "../includes/printf.h"
 
-int	int_recovery(char c, va_list *it)
+size_t	int_recovery(char c, va_list *it)
 {
 	char	*s;
-	int	nb_char;
+	int	size;
 
 	if (c == 'u')
 		s = ft_utoa((va_arg(*it, unsigned int)));
 	else
 		s = ft_itoa(va_arg(*it, int));
 	ft_putstr_fd(s, 1);
-	nb_char = ft_strlen(s);
+	size = ft_strlen(s);
 	free(s);
-	return (nb_char);
+	return (size);
 }
